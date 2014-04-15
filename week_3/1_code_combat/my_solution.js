36180063ba6667e312d00bb4310c0547bb2eb7c2
mulv3959 <mulv3959@users.noreply.github.com>
2014-04-15 17:01:56 -0500

    Update my_solution.js

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -1,16 +1,92 @@
-// I worked on this challenge [by myself, with:]
+I worked on this challenge by myself.
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
- 
- 
- 
- 
- 
- 
- 
+// Rescue Mission
+this.moveDown;
+this.moveDown;
+this.moveRight;
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.moveRight();
+this.moveDown();
+this.attackNearbyEnemy();
+// Grab the Mushroom
+this.moveUp();
+this.moveRight();
+this.moveLeft();
+this.moveUp();
+this.attackNearbyEnemy();
+//Drink Me
+this.attackNearbyEnemy();
+this.moveRight();
+this.moveDown();
+this.moveUp();
+this.moveRight();
+this.attackNearbyEnemy();
+//Taunt the Guards
+this.moveRight();
+this.bustDownDoor();
+this.moveRight();
+this.say("hey there!");
+this.moveLeft();
+this.moveLeft();
+this.say("attack!");
+this.moveRight();
+this.say("follow me");
+this.moveRight();
+this.moveRight();
+this.moveUp();
+this.moveRight();
+this.say("hey there");
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.moveRight();
+//Its a Trap
+this.moveDown();
+this.moveDown();
+this.say("hey there");
+this.moveUp();
+this.moveUp();
+//Taunt
+this.say("Hey!");
+this.say("Do you even lift bro?");
+this.say("you're gunna die clown");
+this.say("cya buddy");
+//Cowardly taunt
+this.moveXY(50, 16);
+this.say("yo");
+this.moveXY(63, 20);
+this.moveXY(70, 10);
+this.say("I can lure them in here.");
+//Commanding Followers
+this.moveXY(49, 66);
+this.moveXY(60, 63);
+this.moveXY(75, 63);
+this.say("Hail, friends!");
+this.say('follow');
+this.moveXY(61,42);
+this.say('attack');
+//Mobile Artillery
+this.moveXY(30, 26);  
+this.attackXY(46, 5); 
+this.moveXY(61,35);
+this.attackXY(68,53);
+this.attackXY(66,50);
+this.moveXY(66,53);
+this.attackXY(49,66);
+this.attackXY(62, 60); 
  
  
 // Reflection:
-// Write your reflection here.
+What is this referring to? Think programming-wise rather than in the terms of the game.
+o	 “this.” Refers to the object that you are commanding. So in our case it was the Thorin the soldier.
+•	What does the () do in JavaScript?
+o	 The parenthesis hold the value of the command. So if the comman was “this.moveXY(45,50)” the numbers in the parenthesis are where our object would move to.
+•	What is the point of the semicolons?
+o 	Separates commands
+
+I accidently created a new my_solution.js page before seeing this so I deleted my pseudo code and replaced it with my real code as I did the challenge.
