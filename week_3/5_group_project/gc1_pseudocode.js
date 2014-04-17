485a6d4b50f0b565944585d2dbf68dd54d6a6cb5
mulv3959 <mulv3959@users.noreply.github.com>
2014-04-17 15:18:50 -0500

    Update and rename my_solution.js to gc1_pseudocode.js

---
+++ week_3/5_group_project/gc1_pseudocode.js
@@ -0,0 +1,14 @@
+sum = function(parameters){code} //create a function called sum
+sum = function(array){return calculated total of all numbers within 'array'}
+sum = function(array){return calculated total of all numbers within 'array'}
+mean = function(parameters){code}
+mean = function(array){return the sum of array divided by length of array} //function 'mean' should return the average of all numbers within an array
+mean = function(array){array.sum divided by array.length}
+median = function(parameters){code}
+median = function(array){
+	If array.length is odd 
+	then function 'median' should return exact middle value of the array}
+median = function(array){
+	If array.length is even
+	then function 'median' should return the sum of the middle two values divided by two
+}
