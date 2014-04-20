9714885e584d7143671f400db3503d5d01062ff3
mulv3959 <mulv3959@users.noreply.github.com>
2014-04-20 14:10:00 -0500

    Update our_solution.js

--- week_3/8_gps1_2/our_solution.js
+++ week_3/8_gps1_2/our_solution.js
@@ -1,8 +1,8 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1. Johnny Mulvahill
+//  2. Simon Gondeck
 
 
 // 1. "YOU SIGNED... WHO?!"
@@ -20,11 +20,25 @@
 
 //YOUR CODE HERE!
 
+function Client(name, age, quote){
+    this.name = name
+    this.age = age
+    this.quote = quote
+    this.sayHi = function(){
+    return "Hi this is " + this.name + ". I am " + this.age + "years old and let me tell ya " + this.quote
+    }
+}
+
+
+var AdamSandler = new Client ("Adam Sandler", 47, "That's your home! Are you too good for your home?!");
+var KristenBell = new Client ("Kristen Bell", 33, "Do you wanna build a snowman?");
+var JimCarrey = new Client ("Jim Carrey", 52, "...So you're tell me there's a chance? YEAH!");
+var JamesFranco = new Client("James Franco", 36, "I'm a big cardigan sweater guy.");
+
 var shooterMcGavin = new Client("Shooter McGavin", 48, "Just stay out of my way... or you'll pay. Listen to what I say.");
 shooterMcGavin.constructor === Client;
 shooterMcGavin.age === 48;
 shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to what I say.";
-
 
 
 
